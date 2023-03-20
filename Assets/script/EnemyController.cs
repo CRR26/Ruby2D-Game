3958f31563978677c4bbd404052b91069339d7ef
CRR26 <crodriguez25@aoiths.org>
2023-03-20 08:59:53 -0400

    gameerubyy

--- Assets/script/EnemyController.cs
+++ Assets/script/EnemyController.cs
@@ -11,12 +11,15 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    Animator animator; 
+    bool broken;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,20 +33,35 @@ public class EnemyController : MonoBehaviour
             timer = changeTime;
         }
        Vector2 position = rigidbody2D.position;
+    }
+     
+     void FixedUpdate()
+    {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
+        
+        Vector2 position = rigidbody2D.position;
         
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
         
-        rigidbody2D.MovePosition(position); 
+        rigidbody2D.MovePosition(position);
     }
-     
-     void OnCollisionEnter2D(Collision2D other)
+    
+    void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController >();
 
@@ -52,4 +70,14 @@ public class EnemyController : MonoBehaviour
             player.ChangeHealth(-1);
         }
     }
+    
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
+    }
+    
 }
